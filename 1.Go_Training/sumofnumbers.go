c2ed4f3e2ae2dbf097363219cba7cf7a561fc5ad
Banu Prakash <bprakash@rsamindia.local>
2020-01-27 17:30:32 +0530

    Sum of assginment completed

---
+++ 1.Go_Training/sumofnumbers.go
@@ -0,0 +1,60 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	number1:=22
+	number2:=33
+	number3:=44
+	sumOf2Numbersvalue:=sumOf2Numbers(number1,number2)
+	fmt.Printf("Sum of numbers %v and %v is %v", number1,number2,sumOf2Numbersvalue)
+	sumOf3Numbersvalue:=sumOf3Numbers(number1,number2,number3)
+	fmt.Printf("\nSum of numbers %v, %v and %v is %v", number1,number2,number3,sumOf3Numbersvalue)
+}
+func sumOf2Numbers(value1,value2 int)int{
+	sum:=value1+value2
+	return sum
+}
+func sumOf3Numbers(value1,value2,value3 int)int{
+	sum:=value1+value2+value3
+	return sum
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
